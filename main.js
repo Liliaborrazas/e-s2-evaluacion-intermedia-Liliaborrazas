3941e49015b0e3518195047cef964eb714bf5d83
lilia <liliaborrazas14@gmail.com>
2018-12-03 08:36:51 +0100

    random

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use strict'
+const introduce = document.querySelector('#write');
+const btn = document.querySelector('.btn');
+const number = document.querySelector('.number');
+const text = document.querySelector('.text');
+
+
+function dameElRandom(num){
+    return Math.floor(Math.random()*num);       
+};
+const resultadoRandom = dameElRandom(100);
+console.log(resultadoRandom);
+
+
+
+// 1. crear una maquetación mínima con el input, el botón, el espacio para feedback y el espacio para el contador de intentos
+// 2. generar un número aleatorio con la ayuda de Math.random y Math.ceil, y mostrarlo por la consola (podéis usar la función de este codepen)
+// 3. al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
+// 4. comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el
+// feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS GANADO, CAMPEONA!")
+// 5. actualizar el contador de intentos cada vez que el usuario pruebe
+// ¡A jugar!
